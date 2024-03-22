f22e8e737d4430daebcf517c26f2751526b08695
Pedro Cavalheiro <pedro@cavalheiro.eu>
2024-03-22 23:42:06 +0000

    Added TypeScript types

--- index.ts
+++ index.ts
@@ -7,19 +7,23 @@ import {
   subtractTwoNumbers,
 } from './math.handler';
 
-const app = express();
+const app: express.Application = express();
 
-const operations = {
-  'sum': addTwoNumbers,
+interface Operations {
+  [key: string]: (a: number, b: number) => number;
+}
+
+const operations: Operations = {
+  'add': addTwoNumbers,  // Ensure your math handlers are properly typed in their respective files
   'div': divideTwoNumbers,
   'subtract': subtractTwoNumbers,
   'multiply': multiplyTwoNumbers,
   'power': powerTwoNumbers,
 };
 
-// A helper function for validating numeric input
 const validateNumbers = (a: string, b: string, res: express.Response): boolean => {
-  if (isNaN(Number(a)) || isNaN(Number(b))) {
+  const numRegex: RegExp = /^-?\d+\.?\d*$/;
+  if (!numRegex.test(a) || !numRegex.test(b)) {
     res.status(400).json({ error: 'Invalid input, numbers required.' });
     return false;
   }
@@ -29,18 +33,19 @@ const validateNumbers = (a: string, b: string, res: express.Response): boolean =
 app.get('/:operation/:a/:b', (req: express.Request, res: express.Response) => {
   const { operation, a, b } = req.params;
 
-  if (!validateNumbers(a, b, res)) return;
-  if (operation === 'div' && Number(b) === 0) {
-    res.status(400).json({ error: 'Division by zero is not allowed.' });
-    return;
-  }
   const operationFunction = operations[operation];
   if (!operationFunction) {
     res.status(404).json({ error: 'Operation not supported.' });
     return;
   }
 
-  const result = operationFunction(Number(a), Number(b));
+  if (!validateNumbers(a, b, res)) return;
+  if (operation === 'div' && Number(b) === 0) {
+    res.status(400).json({ error: 'Division by zero is not allowed.' });
+    return;
+  }
+
+  const result: number = operationFunction(Number(a), Number(b));
   res.json({
     message: `${operation.charAt(0).toUpperCase() + operation.slice(1)} Operation`,
     operation: 'success',
@@ -50,10 +55,10 @@ app.get('/:operation/:a/:b', (req: express.Request, res: express.Response) => {
   });
 });
 
-app.get('/', (_req, res) => {
+app.get('/', (_req: express.Request, res: express.Response) => {
   res.json({
     message: 'Welcome to the Math API',
-    endpoints: Object.keys(operations).map(op => ({
+    endpoints: Object.keys(operations).map((op: string) => ({
       path: `/${op}/:a/:b`,
       description: `Performs the ${op} operation on two numbers.`,
     })),
